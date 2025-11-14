c76c916d93628d5fb9eb69e63449576454567b95
abya <abyalaxx@gmail.com>
2025-11-14 10:15:00 +0700

    update terbaru

---
+++ playground/Main.java
@@ -0,0 +1,13 @@
+import vars.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        Arrays arrData = new Arrays();
+        Arrays secondArrData = new Arrays(6);
+
+        System.out.println(arrData);
+
+    }
+}
---
+++ playground/vars/Arrays.java
@@ -0,0 +1,56 @@
+package vars;
+
+public class Arrays<T> {
+    private Object[] data;
+    private int size;
+    private int capacity;
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    // konstruktor default
+    public Arrays() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    // konstruktor dengan kapasitas custom
+    public Arrays(int capacity) {
+        if (capacity <= 0) {
+            capacity = DEFAULT_CAPACITY;
+        }
+        this.capacity = capacity;
+        this.data = new Object[capacity];
+        this.size = 0;
+    }
+
+    // menambah elemen di akhir
+    public void add(T value) {
+        data[size++] = value;
+    }
+
+    // ambil elemen
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    // ubah elemen
+    public void set(int index, T value) {
+        data[index] = value;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+    public void print() {
+        System.out.print("[");
+        for (int i = 0; i < size; i++) {
+            System.out.print(data[i]);
+            if (i < size - 1)
+                System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
